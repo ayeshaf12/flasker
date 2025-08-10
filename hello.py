38f59e1e1e93dbf7374fe43be088925b898901b2
Tasha <ayeshaf12@gmail.com>
2025-08-10 04:24:56 -0500

    Connecting form with data

--- hello.py
+++ hello.py
@@ -1,23 +1,60 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, flash, redirect, url_for, send_from_directory
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
+from wtforms import RadioField, StringField, SubmitField
 from wtforms.validators import DataRequired
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
 
 
 app= Flask(__name__)
 # Create secret key to avoid hackers for Wtf
-# app.config['SECRET_KEY']= ""
+app.config['SECRET_KEY']= "CONFIDENTIAL KEY"
 
 
 # Create a form class
+class NamerForm(FlaskForm):
+    id_type = RadioField('ID Type', choices=[('student_id', 'Student ID'), ('course_id', 'Course ID')], validators=[DataRequired()])
+    id_value = StringField('ID Value', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
+# Create namer form
+@app.route('/namerform',methods=['GET','POST'],endpoint='namerform')
+def form():
+    form=NamerForm()
+    if form.validate_on_submit():
+        id_type = form.id_type.data
+        id_value = form.id_value.data
+        if id_type == 'student id':
+            return redirect(url_for('student_details', student_id=id_value))
+        elif id_type == 'course':
+            return redirect(url_for('course_statistics', course_id=id_value))
+        # form.id_value.data = ''
+        # flash("Form submitted successfully !!! ")
+    return render_template("namerform.html",form=form)
+
+#################################
+# @app.route('/student/<student_id>')
+# def student_details(student_id):
+#     f=open('data.csv',r)
+#     data_list=f.readlines()
+#     student_list=[]
+#     course_list=[]
+#     for each_line in data_list[1:]:
+#             again_list=each_line.split(',')
+#             if F_ID =='st_id' and input_Number== again_list[0].strip():
+#                 student_list.append(each_line)
+#             else:
+#                 if F_ID == 'c_id' and input_Number== again_list[1].strip():
+#                     course_list.append(each_line)
+#############################
 
 
-
-@app.route('/')
-def index():
-    return render_template("index.html")
+# @app.route('/')
+# def index():
+#     return render_template("index.html")
 
 # @app.route('/user/<name>')
 # def user(name):
